84ea57a17b3ed329d5cbb8a1eb449c3cbeaf60be
eoinlynchcodes <eoin95lynch@gmail.com>
2020-03-18 15:34:08 +0000

    Some code...

---
+++ schemes/scheme-model.js
@@ -0,0 +1,33 @@
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+};
+
+function find(){
+
+}
+
+function findById(id){
+    db('steps')
+    .join('schemes')
+}
+
+function findSteps(id){
+
+}
+
+function add(user){
+
+}
+
+function update(changes, id){
+
+}
+
+function remove(id){
+
+}
